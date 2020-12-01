a92c10b360228d2ff745cd35a6ecdd72a1ff0d83
aniagud <anna.gudelajtis@gmail.com>
2020-12-01 15:09:55 +0100

    Files and ILPD Data added

---
+++ mmo.py
@@ -0,0 +1,11 @@
+"""
+Miękkie metody obliczeniowe
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.neighbors import DistanceMetric
+from sklearn.metrics import accuracy_score
+from sklearn.decomposition import PCA
+from sklearn.model_selection import RepeatedStratifiedKFold
+np.set_printoptions(suppress=True)
